80a3c8836461633786153b2883084396f3bedf36
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2018-08-31 17:46:45 +0300

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="section">
         <header class="section-header">
-            <h2 class="section-header__title">Наши услуги</h2>
+            <h2 class="section-header__title">Новости логистики</h2>
             <p class="section-header__subtitle">Господа выбранный нами инновационный путь создает предпосылки.</p>
         </header>
     </section>
